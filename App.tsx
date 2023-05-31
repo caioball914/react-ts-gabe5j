32cd5d77a0efefd350b9be0a1e33528eada3c1ab
caioball914 <117281404+caioball914@users.noreply.github.com>
2023-05-31 03:55:30 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import './style.css';
+import { ChartComponent } from './ChartComponent';
+
+const initialData = [
+  { time: '2018-12-22', value: 32.51 },
+  { time: '2018-12-23', value: 31.11 },
+  { time: '2018-12-24', value: 27.02 },
+  { time: '2018-12-25', value: 27.32 },
+  { time: '2018-12-26', value: 25.17 },
+  { time: '2018-12-27', value: 28.89 },
+  { time: '2018-12-28', value: 25.46 },
+  { time: '2018-12-29', value: 23.92 },
+  { time: '2018-12-30', value: 22.68 },
+  { time: '2018-12-31', value: 22.67 },
+];
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <ChartComponent data={initialData}></ChartComponent>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-gabe5j
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-gabe5j)
